ea51dd169486134e016cf21765985dc939e4f38e
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2019-10-19 21:01:11 -0400

    Initial Commit

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,32 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Juan J. Mendoza on 10/19/19.
+//  Copyright © 2019 Juan J. Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
