f20c5f407912678b98a115dff8be5c685aa2ceaf
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2019-10-25 16:41:04 -0400

    finished grid view

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+ //
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Juan J. Mendoza on 10/25/19.
+//  Copyright © 2019 Juan J. Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+ }
